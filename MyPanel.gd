bce869329d4c8593512f76ccad19a392e4544001
Nguyễn Anh Quân <naquan2112@gmail.com>
2017-01-16 11:24:19 +0700

    repo initialized, starting pong game.

---
+++ MyPanel.gd
@@ -0,0 +1,15 @@
+extends Panel
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+
+func _on_Button_pressed():
+	get_node("Label").set_text("HELLO!")
+	pass # replace with function body
